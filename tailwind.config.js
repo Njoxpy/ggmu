4f87b500988a3a1c101ff3fd466dccf23d26a5c6
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-05-30 09:43:06 +0300

    added custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        red: '#DA291C',         // Primary Red
+        darkRed: '#A50000',     // Dark Red
+        white: '#FFFFFF',       // White
+        black: '#000000',       // Black
+        gold: '#F3D459',        // Gold
+        gray: '#333333',        // Dark Gray
+    }
     },
   },
   plugins: [],
