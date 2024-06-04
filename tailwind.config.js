478b7f221200e1968560ff6da83be71f18560d19
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-06-04 08:44:33 +0300

    added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
-        red: '#DA291C',         // Primary Red
-        darkRed: '#A50000',     // Dark Red
-        white: '#FFFFFF',       // White
-        black: '#000000',       // Black
-        gold: '#F3D459',        // Gold
-        gray: '#333333',        // Dark Gray
-    }
+        red: "#DA291C", // Primary Red
+        darkRed: "#A50000", // Dark Red
+        white: "#FFFFFF", // White
+        black: "#000000", // Black
+        gold: "#F3D459", // Gold
+        gray: "#333333", // Dark Gray
+        textGray: "#D3D3D3", // text color gray footer text
+        textHover: "#A6261C", // text hover lighter red
+        lightGray: "#f5f5f5", // light gray
+      },
+      backgroundImage:{
+        'helloBg': 'url("../components/assets/bg.jpg")'
+
+        // add other photos for legends
+      }
     },
   },
   plugins: [],
-}
+};
